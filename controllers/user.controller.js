51d47eabf6a0872c54328db78ee86ead623f9ae9
Ankur <as797007@gmail.com>
2022-03-04 20:09:27 +0530

    fixing

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -376,7 +376,7 @@ exports.forgotPassword = async (req, res) => {
   <td valign="top" align="center" style="padding:0;Margin:0;width:560px">
   <table width="100%" cellspacing="0" cellpadding="0" role="presentation" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px">
   <tr>
-  <td align="center" style="padding:0;Margin:0"><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, 'helvetica neue', helvetica, sans-serif;line-height:21px;color:#040303;font-size:14px"><h1>Your password Reset Link </h1><br> https://craftwill-m2.vercel.app/resetpassword/${userData._id} <br> <h3> The link will get expired after 10 minutes </h3>&nbsp;</p></td>
+  <td align="center" style="padding:0;Margin:0"><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, 'helvetica neue', helvetica, sans-serif;line-height:21px;color:#040303;font-size:14px"><h1>Your password Reset Link </h1><br> https://craftwill-m3.vercel.app/resetpassword/${userData._id} <br> <h3> The link will get expired after 10 minutes </h3>&nbsp;</p></td>
   </tr>
   </table></td>
   </tr>
