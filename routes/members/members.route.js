f9436827079f498d1f29da8f5dd6ae980f2e2ab7
Ankur Singh <as797007@gmail.com>
2022-05-06 17:49:50 +0530

    newe

--- routes/members/members.route.js
+++ routes/members/members.route.js
@@ -3,6 +3,7 @@ const { authenticateToken } = require("../../JsonWebToken/jwt");
 const router = express.Router();
 const membersController = require ("../../controllers/members/members.controller")
 ///
+////
 router.post("/createMember",authenticateToken,membersController.createMember)
 router.get ("/getMembers",authenticateToken,membersController.getMembers)
 router.post("/filterMembers",authenticateToken,membersController.membersFilter)
